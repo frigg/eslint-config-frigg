405b37b7bc34ef7a8cf0516ca959890b7f5d425c
Rolf Erik Lekang <me@rolflekang.com>
2015-10-27 21:28:31 +0100

    Add a few react rules

--- react.js
+++ react.js
@@ -9,6 +9,15 @@ module.exports = {
   ecmaFeatures: _.assign(base.ecmaFeatures, {
     'jsx': true,
   }),
+  
   rules: _.assign(base.rules, {
+    'jsx-quotes': [2, 'prefer-single'],
+    'react/display-name': [1, { 'acceptTranspilerName': true }],
+    'react/jsx-boolean-value': [2, 'always'],
+    'react/jsx-no-undef': 2,
+    'react/jsx-uses-react': 2,
+    'react/no-multi-comp': 2,
+    'react/no-unknown-property': 2,
+    'react/prop-types': 2,
   }),
 };
